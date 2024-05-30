f20ac9e2316489556aae24ff554c958273420c57
nurajsp <75216324+nurajsp@users.noreply.github.com>
2024-05-30 19:26:18 +0530

    known error

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 
 require('dotenv').config();
